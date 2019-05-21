30f5fd438436c7ef0a7c6197ecb3d51cab28ddf2
haurenburu <haurenburu@protonmail.com>
2019-05-21 16:18:42 -0300

    List 07 Exercise 02

--- List07/Q02.c
+++ List07/Q02.c
@@ -1,5 +1,6 @@
 /* Faça um programa que leia uma stringe a imprima de trás para frente */
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	char palavra[20];
@@ -7,7 +8,7 @@ int main() {
 	printf("Digite a string: ");
 	fgets(palavra, 20, stdin);
 	
-	for (int i = 20; i >= 0; i--) {
+	for (int i = strlen(palavra); i >= 0; i--) {
 		printf("%c", palavra[i]);
 	}
 	
