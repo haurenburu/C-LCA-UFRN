d6b0c490197ce0246fc7a2449452cea306b7f227
haurenburu <haurenburu@protonmail.com>
2019-05-21 16:14:21 -0300

    List 07 Exercise 03

---
+++ List07/Q03.c
@@ -0,0 +1,16 @@
+/* Faça um programa que leia uma string e a inverta. A string invertida deve ser armazenada na mesma variável. Em seguida, imprima a string invertida */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char palavra[20], palavraInvertida[20];
+	
+	printf("Digite a string: ");
+	fgets(palavra, 20, stdin);
+	
+	strrev(palavra);
+	
+	printf("%s", palavra);
+	
+	return 0;
+}
